f3de27183bc81613ca45508fde15a74de93e6645
Ioana Culic <ioana_mculic@yahoo.com>
2020-04-02 12:55:22 +0300

    bonus

--- devoir/devoir3/verify/6-bonus/users.sh
+++ devoir/devoir3/verify/6-bonus/users.sh
@@ -1,4 +1,4 @@
-sleep 1.2 && cat < users | cut -d ":" -f 5 > users_names & sleep 0.2 && cat < /etc/passwd | grep "/bin/false" > users & sleep 0.6 && cat < /etc/passwd | grep "/sbin/nologin" >> users
+cat < /etc/passwd | grep "/bin/false" > users && cat < /etc/passwd | grep "/sbin/nologin" >> users && cat < users | cut -d ":" -f 5 > users_names
 sleep 1.7
 echo "Users"
 cat users
