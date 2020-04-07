54b55c63ab061f0478d9c0b3c76d8ac13223eaff
Ioana Culic <ioana_mculic@yahoo.com>
2020-04-07 21:53:17 +0300

    tp08

---
+++ tp/tp08/3-numbers/main.py
@@ -0,0 +1,11 @@
+import time
+
+def thread_function ():
+    # TODO - implement function
+
+# TODO - create thread
+
+# main process
+for i in range (2):
+    print ('main {}'.format (i))
+    time.sleep (1)
